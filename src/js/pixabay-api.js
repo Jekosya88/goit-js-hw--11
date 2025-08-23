0f4cdfafbda848272255ccd6ef1a102fa7fd54a2
Yevhenii <zyv0007@gmail.com>
2025-08-23 15:58:04 +0200

    imstall

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+const API_KEY = "51904877-a497cebd24b125b23f704ea34";
 const BASE_URL = "https://pixabay.com/api/";
-const API_key = "51904877-a497cebd24b125b23f704ea34";
+
 
 export async function getImagesByQuery(query) {
   const params = {
-    key: API_key,
+    key: API_KEY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
